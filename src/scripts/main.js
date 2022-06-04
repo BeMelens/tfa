ea2f4f4fc0d5d83165c8aa59dad569f30564eadb
Melvyn Ulens <mel.ulens@gmail.com>
2022-06-04 23:41:29 +0200

    Add burger menu and finished CSS grid

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,3 +5,15 @@ let date = new Date();
 let year = date.getFullYear();
 let outputCopyright = document.querySelector("#js__copyright");
 outputCopyright.innerHTML = year;
+
+//Burger Menu
+let button = document.querySelector(".bg_menu");
+let nav = document.querySelector(".nav");
+
+button.addEventListener("click", (e)=>{
+    if (nav.classList.contains("nav--open")){
+        nav.classList.remove("nav--open");
+    }else{
+        nav.classList.add("nav--open");
+    }
+});
