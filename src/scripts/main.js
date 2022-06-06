4dd622454f359c3f1c1befed90308b881bd8fe57
Melvyn Ulens <mel.ulens@gmail.com>
2022-06-06 23:05:23 +0200

    Patch error on JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,124 +23,131 @@ button.addEventListener("click", (e)=>{
 });
 
 //GSAP
-gsap.registerPlugin(ScrollTrigger);
+
+let gsapIsHere = document.querySelector("#gsap_1");
+
+if (gsapIsHere){
+
+    gsap.registerPlugin(ScrollTrigger);
 
 
-ScrollTrigger.matchMedia({
+    ScrollTrigger.matchMedia({
 
 
-    //Mobile
-    "(max-width: 399px)": function() {
+        //Mobile
+        "(max-width: 399px)": function() {
 
-        gsap.from("#gsap_1", {
-            position: "relative",
-            right: "-400",
-            scrollTrigger: {
-                trigger: "#gsap_1",
-                start: "bottom 100%",
-                end: "top 30%",
-                scrub:0.5
-            },
-        });
+            gsap.from("#gsap_1", {
+                position: "relative",
+                right: "-400",
+                scrollTrigger: {
+                    trigger: "#gsap_1",
+                    start: "bottom 100%",
+                    end: "top 30%",
+                    scrub:0.5
+                },
+            });
 
-        gsap.from("#gsap_2", {
-            position: "relative",
-            left: "-400",
-            scrollTrigger: {
-                trigger: "#gsap_2",
-                start: "bottom 100%",
-                end: "top 30%",
-                scrub:0.5
-            },
-        });
+            gsap.from("#gsap_2", {
+                position: "relative",
+                left: "-400",
+                scrollTrigger: {
+                    trigger: "#gsap_2",
+                    start: "bottom 100%",
+                    end: "top 30%",
+                    scrub:0.5
+                },
+            });
 
-        gsap.from("#gsap_3", {
-            position: "relative",
-            right: "-400",
-            scrollTrigger: {
-                trigger: "#gsap_3",
-                start: "bottom 100%",
-                end: "top 30%",
-                scrub:0.5
-            }
-        });
+            gsap.from("#gsap_3", {
+                position: "relative",
+                right: "-400",
+                scrollTrigger: {
+                    trigger: "#gsap_3",
+                    start: "bottom 100%",
+                    end: "top 30%",
+                    scrub:0.5
+                }
+            });
 
-    },
+        },
 
-    //Mobile+ / Tablette
-    "(min-width: 400px) and (max-width: 1299px)": function() {
+        //Mobile+ / Tablette
+        "(min-width: 400px) and (max-width: 1299px)": function() {
 
-        gsap.from("#gsap_1", {
-            position: "relative",
-            right: "-600",
-            scrollTrigger: {
-                trigger: "#gsap_1",
-                start: "bottom 100%",
-                end: "top 30%",
-                scrub:0.5
-            },
-        });
+            gsap.from("#gsap_1", {
+                position: "relative",
+                right: "-600",
+                scrollTrigger: {
+                    trigger: "#gsap_1",
+                    start: "bottom 100%",
+                    end: "top 30%",
+                    scrub:0.5
+                },
+            });
 
-        gsap.from("#gsap_2", {
-            position: "relative",
-            left: "-600",
-            scrollTrigger: {
-                trigger: "#gsap_2",
-                start: "bottom 100%",
-                end: "top 30%",
-                scrub:0.5
-            },
-        });
+            gsap.from("#gsap_2", {
+                position: "relative",
+                left: "-600",
+                scrollTrigger: {
+                    trigger: "#gsap_2",
+                    start: "bottom 100%",
+                    end: "top 30%",
+                    scrub:0.5
+                },
+            });
 
-        gsap.from("#gsap_3", {
-            position: "relative",
-            right: "-600",
-            scrollTrigger: {
-                trigger: "#gsap_3",
-                start: "bottom 100%",
-                end: "top 30%",
-                scrub:0.5
-            }
-        });
+            gsap.from("#gsap_3", {
+                position: "relative",
+                right: "-600",
+                scrollTrigger: {
+                    trigger: "#gsap_3",
+                    start: "bottom 100%",
+                    end: "top 30%",
+                    scrub:0.5
+                }
+            });
 
-    },
+        },
 
-    //Desktop
-    "(min-width: 1300px)": function() {
+        //Desktop
+        "(min-width: 1300px)": function() {
 
-        gsap.to("#gsap_1", {
-            position: "relative",
-            bottom: "-200",
-            scrollTrigger: {
-                trigger: "#gsap_1",
-                start: "top 40%",
-                end: "bottom 20%",
-                scrub:0.5
-            },
-        });
+            gsap.to("#gsap_1", {
+                position: "relative",
+                bottom: "-200",
+                scrollTrigger: {
+                    trigger: "#gsap_1",
+                    start: "top 40%",
+                    end: "bottom 20%",
+                    scrub:0.5
+                },
+            });
 
-        gsap.to("#gsap_2", {
-            position: "relative",
-            bottom: "-100",
-            scrollTrigger: {
-                trigger: "#gsap_2",
-                start: "top 50%",
-                end: "bottom 50%",
-                scrub:0.5
-            },
-        });
+            gsap.to("#gsap_2", {
+                position: "relative",
+                bottom: "-100",
+                scrollTrigger: {
+                    trigger: "#gsap_2",
+                    start: "top 50%",
+                    end: "bottom 50%",
+                    scrub:0.5
+                },
+            });
 
-        gsap.to("#gsap_3", {
-            position: "relative",
-            bottom: "-300",
-            scrollTrigger: {
-                trigger: "#gsap_3",
-                start: "top 20%",
-                end: "bottom 0%",
-                scrub:0.5
-            }
-        });
+            gsap.to("#gsap_3", {
+                position: "relative",
+                bottom: "-300",
+                scrollTrigger: {
+                    trigger: "#gsap_3",
+                    start: "top 20%",
+                    end: "bottom 0%",
+                    scrub:0.5
+                }
+            });
 
-    }
+        }
     
-});
+    });
+
+};
